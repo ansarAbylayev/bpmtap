d8e8b6d46b2033c02d88e75101e66e702a19243a
ansarAbylayev <ansarerlanovich2006@gmail.com>
2026-08-12 23:10:26 -0700

    add bpm endpoint

--- src/main/java/com/example/bpmtap/BpmController.java
+++ src/main/java/com/example/bpmtap/BpmController.java
@@ -1,6 +1,8 @@
 package com.example.bpmtap;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,13 @@ public class BpmController {
 
     @GetMapping("/api/ping")
     public String ping() {
-        return "работает";
+        return "working";
     }
+    @PostMapping("/api/bpm")
+    public BpmResponse calculateBPM(@RequestBody int[] taps){
+        BpmCalculator calc = new BpmCalculator();
+        int bpm = calc.getBpm(taps);
+        return new BpmResponse(bpm, taps.length);
+    }
+
 }
